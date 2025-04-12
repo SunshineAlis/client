99161e52dbc0de348a8a2b224d8632a58e807796
SunshineAlis <bumaam@yahoo.com>
2025-04-12 13:54:46 +0800

    1

--- src/app/components/CoverImg.tsx
+++ src/app/components/CoverImg.tsx
@@ -13,7 +13,7 @@ export const CoverImage = ({ page, className }: imageProps) => {
     useEffect(() => {
         const fetchImage = async () => {
             if (!page) {
-                setUrl("");
+                setUrl("/BG.png");
                 return;
             }
             try {
